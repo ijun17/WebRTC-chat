6818cc974f20336b5bb75196c64e6d2baad8e975
ijun17 <ijun17@naver.com>
2023-07-27 10:43:37 +0900

    a

--- res/js/WebSocket.js
+++ res/js/WebSocket.js
@@ -3,24 +3,25 @@ let Multi = {
     connect:false,
     serverOn:false,
     gameOn:false,
-    IP:"211.194.75.223",
+    IP:"port-0-webrtc-test-eg4e2alkj86xoo.sel4.cloudtype.app/",
     playerNum:'n', //n:익명 0:1번째플레이어 1:2번째플레이어
     makeWebSocket: function () {
-        this.ws = new WebSocket("ws://"+Multi.IP+":777");
+        this.ws = new WebSocket("ws://"+Multi.IP);
         // 연결이 수립되면 서버에 메시지를 전송한다
         this.ws.onopen = function (event) {
-            Multi.serverOn=true;
+            //Multi.serverOn=true;
+            console.log("onopen")
         }
         // 서버로 부터 메시지를 수신한다
         this.ws.onmessage = function (event) {
-            //console.log("SERVER: "+event.data);
-            if(Multi.gameOn)Multi.requestHandlerGameOn(event.data);
-            else Multi.requestHandlerGameOff(event.data);
+            console.log("onmessage: "+event.data);
         }
         // error event handler
         this.ws.onerror = function (event) {
-            Multi.serverOn=false;
-            if(Multi.connect)Multi.makeWebSocket();
+            console.log("onerror")
+            // Multi.serverOn=false;
+            // if(Multi.connect)Multi.makeWebSocket();
+
         }
     },
     connectOn:function(){
@@ -35,3 +36,5 @@ let Multi = {
         this.playerNum='n';
     }
 }
+
+Multi.makeWebSocket();
